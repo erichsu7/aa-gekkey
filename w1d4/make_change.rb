29e246c3daf8a9b75b85871cf3a25bb6e80f7e9f
gekkey <mgekkey@gmail.com>
2014-12-14 18:56:32 +0000

    working through the git tutorial

--- w1d4/make_change.rb
+++ w1d4/make_change.rb
@@ -1,10 +1,8 @@
-def make_change(amount, coins = [10, 7, 1])
+def make_change(target, coins = [10, 7, 1])
   valid_coins = []
   coins.each do |coin|
-    next if coin > amount
-    valid_coins << [coin] + (amount - coin != 0 ? make_change(amount - coin) : [] )
+    next if coin > target
+    valid_coins << [coin] + (target - coin != 0 ? make_change(target - coin, coins) : [] )
   end
   valid_coins.sort { |a, b| a.length <=> b.length } .first
 end
-
-p make_change(14)
